21493d72415444c9f41ab241f0c39737ad125904
chenyueqin <chenyueqin1010@sina.com>
2023-04-20 16:10:23 +0800

    uplod pages

---
+++ src/components/Chat.vue
@@ -0,0 +1,139 @@
+<template>
+  <div class="q-pa-md row justify-center">
+    <div class="message-content">
+      <q-chat-message v-for="(item, index) in messages" :key="index" :name="item.name" :text="[item.content]"
+        :sent="item.name === 'ME'">
+
+        <template v-slot:avatar>
+          <q-avatar v-if="item.name === 'ME'" color="orange">Me</q-avatar>
+          <q-avatar v-else>
+            <img src="../assets/openai.jpeg" alt="GPT" />
+          </q-avatar>
+        </template>
+      </q-chat-message>
+
+      <q-chat-message v-show="sending" name="GPT" avatar="../assets/openai.jpeg">
+        <q-spinner-dots size="1.5em" />
+      </q-chat-message>
+    </div>
+  </div>
+
+  <q-input class="input-text" ref="inputRef" v-model="text" placeholder="say something..." filled autogrow
+    @keypress.enter.prevent="sendMessage">
+    <template v-slot:append>
+      <q-btn flat round color="primary" icon="send" :disabled="!text.trim()" @click.prevent="sendMessage" />
+    </template>
+  </q-input>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+import { Configuration, OpenAIApi } from "openai";
+import {
+  useQuasar
+} from 'quasar';
+const $q = useQuasar();
+
+const configuration = new Configuration({
+  organization: "org-lccnrzbMqST4GlSb2BdV3N7R",
+  apiKey: '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',
+});
+const openai = new OpenAIApi(configuration);
+
+const text = ref('')
+const messages = ref([]);
+const sending = ref(false);
+let lastText = '';
+
+// 发送请求
+const sendMessage = async () => {
+  if (sending.value) {
+    $q.notify({
+      type: 'warning',
+      message: 'Please wait a moment'
+    });
+    return;
+  }
+  sending.value = true;
+  messages.value.push({
+    name: 'ME',
+    content: text.value
+  });
+  lastText = text.value;
+  text.value = '';
+
+  setTimeout(() => {
+    window.scrollTo(0, document.documentElement.scrollHeight);
+  }, 100)
+
+  const completion = await openai.createChatCompletion({
+    model: "gpt-3.5-turbo",
+    messages: [{ role: "user", content: lastText }],
+  }).catch(err => {
+    $q.notify({
+      type: 'warning',
+      message: 'Please wait a moment'
+    });
+    messages.value.shift();
+  });
+  sending.value = false;
+
+  if(!completion || !completion.data){
+    return;
+  }
+
+  messages.value.push({
+    name: 'GPT',
+    content: completion.data.choices[0].message.content
+  });
+
+  setTimeout(() => {
+    window.scrollTo(0, document.documentElement.scrollHeight);
+  }, 100)
+
+}
+
+</script>
+
+
+<style type="text/css" lang="scss">
+.input-text {
+  width: 100%; 
+  position: fixed;
+  left: 0;
+  bottom: 0;
+  background-color: white;
+}
+
+.q-message-text-content {
+  min-width: 5em;
+  line-height: 1.5em;
+}
+
+.q-message-text:last-child {
+  min-height: 0;
+}
+
+.q-scroll {
+  width: 100% !important;
+  max-height: 100%;
+}
+
+.q-message-received {
+  .q-avatar {
+    margin-right: 5px;
+  }
+}
+
+.q-message-sent {
+  .q-avatar {
+    margin-left: 5px;
+  }
+}
+
+.message-content {
+  width: 100%;
+  padding-bottom: 60px;
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,32 @@
+// FILE: main.js
+
+import { createApp } from 'vue'
+import { Quasar, Notify } from 'quasar'
+
+// Import icon libraries
+import '@quasar/extras/material-icons/material-icons.css'
+
+// Import Quasar css
+import 'quasar/src/css/index.sass'
+
+// Assumes your root component is App.vue
+// and placed in same folder as main.js
+import App from './App.vue'
+
+const myApp = createApp(App)
+
+myApp.use(Quasar, {
+  plugins: {
+    Notify
+  }, // import Quasar plugins and add here
+  config: {
+    notify: {
+      color: 'primary',
+      position: 'top',
+      timeout: 2000
+    }
+  }
+})
+
+// Assumes you have a <div id="app"></div> in your index.html
+myApp.mount('#app')
